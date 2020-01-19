6d121bdd78cc7fb701ec856d96bc162332328332
Mateus Sousa <mateusdevsousa@gmail.com>
2020-01-19 00:47:07 -0300

    Initial Commit

---
+++ In-app-purchase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  In-app-purchase
+//
+//  Created by Mateus Sousa on 19/01/20.
+//  Copyright © 2020 MobApps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
